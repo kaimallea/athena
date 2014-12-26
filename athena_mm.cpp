23c93a84a021ec9a2912056af49700777eef0305
Kai Mallea <kai@spotify.com>
2014-12-26 17:09:46 -0500

    Initial commit

---
+++ athena_mm.cpp
@@ -0,0 +1,306 @@
+/**
+ * vim: set ts=4 sw=4 tw=99 noet :
+ */
+
+#include <stdio.h>
+#include "athena_mm.h"
+
+SH_DECL_HOOK6(IServerGameDLL, LevelInit, SH_NOATTRIB, 0, bool, char const *, char const *, char const *, char const *, bool, bool);
+SH_DECL_HOOK3_void(IServerGameDLL, ServerActivate, SH_NOATTRIB, 0, edict_t *, int, int);
+SH_DECL_HOOK1_void(IServerGameDLL, GameFrame, SH_NOATTRIB, 0, bool);
+SH_DECL_HOOK0_void(IServerGameDLL, LevelShutdown, SH_NOATTRIB, 0);
+SH_DECL_HOOK2_void(IServerGameClients, ClientActive, SH_NOATTRIB, 0, edict_t *, bool);
+SH_DECL_HOOK1_void(IServerGameClients, ClientDisconnect, SH_NOATTRIB, 0, edict_t *);
+SH_DECL_HOOK2_void(IServerGameClients, ClientPutInServer, SH_NOATTRIB, 0, edict_t *, char const *);
+SH_DECL_HOOK1_void(IServerGameClients, SetCommandClient, SH_NOATTRIB, 0, int);
+SH_DECL_HOOK1_void(IServerGameClients, ClientSettingsChanged, SH_NOATTRIB, 0, edict_t *);
+SH_DECL_HOOK5(IServerGameClients, ClientConnect, SH_NOATTRIB, 0, bool, edict_t *, const char*, const char *, char *, int);
+SH_DECL_HOOK2(IGameEventManager2, FireEvent, SH_NOATTRIB, 0, bool, IGameEvent *, bool);
+
+#if SOURCE_ENGINE >= SE_ORANGEBOX
+SH_DECL_HOOK2_void(IServerGameClients, NetworkIDValidated, SH_NOATTRIB, 0, const char *, const char *);
+SH_DECL_HOOK2_void(IServerGameClients, ClientCommand, SH_NOATTRIB, 0, edict_t *, const CCommand &);
+#else
+SH_DECL_HOOK1_void(IServerGameClients, ClientCommand, SH_NOATTRIB, 0, edict_t *);
+#endif
+
+MatchState g_MatchState = WARMUP;
+AthenaPlugin g_AthenaPlugin;
+IServerGameDLL *server = NULL;
+IServerGameClients *gameclients = NULL;
+IVEngineServer *engine = NULL;
+IServerPluginHelpers *helpers = NULL;
+IGameEventManager2 *gameevents = NULL;
+IServerPluginCallbacks *vsp_callbacks = NULL;
+IPlayerInfoManager *playerinfomanager = NULL;
+ICvar *icvar = NULL;
+CGlobalVars *gpGlobals = NULL;
+
+ConVar sample_cvar("sample_cvar", "42", 0);
+
+/**
+ * Something like this is needed to register cvars/CON_COMMANDs.
+ */
+class BaseAccessor : public IConCommandBaseAccessor
+{
+public:
+	bool RegisterConCommandBase(ConCommandBase *pCommandBase)
+	{
+		/* Always call META_REGCVAR instead of going through the engine. */
+		return META_REGCVAR(pCommandBase);
+	}
+} s_BaseAccessor;
+
+PLUGIN_EXPOSE(AthenaPlugin, g_AthenaPlugin);
+bool AthenaPlugin::Load(PluginId id, ISmmAPI *ismm, char *error, size_t maxlen, bool late)
+{
+	PLUGIN_SAVEVARS();
+
+	GET_V_IFACE_CURRENT(GetEngineFactory, engine, IVEngineServer, INTERFACEVERSION_VENGINESERVER);
+	GET_V_IFACE_CURRENT(GetEngineFactory, gameevents, IGameEventManager2, INTERFACEVERSION_GAMEEVENTSMANAGER2);
+	GET_V_IFACE_CURRENT(GetEngineFactory, helpers, IServerPluginHelpers, INTERFACEVERSION_ISERVERPLUGINHELPERS);
+	GET_V_IFACE_CURRENT(GetEngineFactory, icvar, ICvar, CVAR_INTERFACE_VERSION);
+	GET_V_IFACE_ANY(GetServerFactory, server, IServerGameDLL, INTERFACEVERSION_SERVERGAMEDLL);
+	GET_V_IFACE_ANY(GetServerFactory, gameclients, IServerGameClients, INTERFACEVERSION_SERVERGAMECLIENTS);
+	GET_V_IFACE_ANY(GetServerFactory, playerinfomanager, IPlayerInfoManager, INTERFACEVERSION_PLAYERINFOMANAGER);
+
+	gpGlobals = ismm->GetCGlobals();
+
+	META_LOG(g_PLAPI, "Starting plugin.");
+
+	/* Load the VSP listener.  This is usually needed for IServerPluginHelpers. */
+	if ((vsp_callbacks = ismm->GetVSPInfo(NULL)) == NULL)
+	{
+		ismm->AddListener(this, this);
+		ismm->EnableVSPListener();
+	}
+
+	/* Player chat is not emitted by default, so we need to add a listener for it */
+	if (gameevents)
+	{
+		gameevents->AddListener(this, "player_say", true);
+	}
+
+	SH_ADD_HOOK_MEMFUNC(IServerGameDLL, LevelInit, server, this, &AthenaPlugin::Hook_LevelInit, true);
+	SH_ADD_HOOK_MEMFUNC(IServerGameDLL, ServerActivate, server, this, &AthenaPlugin::Hook_ServerActivate, true);
+	SH_ADD_HOOK_MEMFUNC(IServerGameDLL, GameFrame, server, this, &AthenaPlugin::Hook_GameFrame, true);
+	SH_ADD_HOOK_MEMFUNC(IServerGameDLL, LevelShutdown, server, this, &AthenaPlugin::Hook_LevelShutdown, false);
+	SH_ADD_HOOK_MEMFUNC(IServerGameClients, ClientActive, gameclients, this, &AthenaPlugin::Hook_ClientActive, true);
+	SH_ADD_HOOK_MEMFUNC(IServerGameClients, ClientDisconnect, gameclients, this, &AthenaPlugin::Hook_ClientDisconnect, true);
+	SH_ADD_HOOK_MEMFUNC(IServerGameClients, ClientPutInServer, gameclients, this, &AthenaPlugin::Hook_ClientPutInServer, true);
+	SH_ADD_HOOK_MEMFUNC(IServerGameClients, SetCommandClient, gameclients, this, &AthenaPlugin::Hook_SetCommandClient, true);
+	SH_ADD_HOOK_MEMFUNC(IServerGameClients, ClientSettingsChanged, gameclients, this, &AthenaPlugin::Hook_ClientSettingsChanged, false);
+	SH_ADD_HOOK_MEMFUNC(IServerGameClients, ClientConnect, gameclients, this, &AthenaPlugin::Hook_ClientConnect, false);
+	SH_ADD_HOOK_MEMFUNC(IServerGameClients, ClientCommand, gameclients, this, &AthenaPlugin::Hook_ClientCommand, false);
+	SH_ADD_HOOK_MEMFUNC(IGameEventManager2, FireEvent, gameevents, this, &AthenaPlugin::Hook_FireEvent, false);
+
+	ENGINE_CALL(LogPrint)("All hooks started!\n");
+
+#if SOURCE_ENGINE >= SE_ORANGEBOX
+	g_pCVar = icvar;
+	ConVar_Register(0, &s_BaseAccessor);
+#else
+	ConCommandBaseMgr::OneTimeInit(&s_BaseAccessor);
+#endif
+
+	return true;
+}
+
+bool AthenaPlugin::Unload(char *error, size_t maxlen)
+{
+	SH_REMOVE_HOOK_MEMFUNC(IServerGameDLL, LevelInit, server, this, &AthenaPlugin::Hook_LevelInit, true);
+	SH_REMOVE_HOOK_MEMFUNC(IServerGameDLL, ServerActivate, server, this, &AthenaPlugin::Hook_ServerActivate, true);
+	SH_REMOVE_HOOK_MEMFUNC(IServerGameDLL, GameFrame, server, this, &AthenaPlugin::Hook_GameFrame, true);
+	SH_REMOVE_HOOK_MEMFUNC(IServerGameDLL, LevelShutdown, server, this, &AthenaPlugin::Hook_LevelShutdown, false);
+	SH_REMOVE_HOOK_MEMFUNC(IServerGameClients, ClientActive, gameclients, this, &AthenaPlugin::Hook_ClientActive, true);
+	SH_REMOVE_HOOK_MEMFUNC(IServerGameClients, ClientDisconnect, gameclients, this, &AthenaPlugin::Hook_ClientDisconnect, true);
+	SH_REMOVE_HOOK_MEMFUNC(IServerGameClients, ClientPutInServer, gameclients, this, &AthenaPlugin::Hook_ClientPutInServer, true);
+	SH_REMOVE_HOOK_MEMFUNC(IServerGameClients, SetCommandClient, gameclients, this, &AthenaPlugin::Hook_SetCommandClient, true);
+	SH_REMOVE_HOOK_MEMFUNC(IServerGameClients, ClientSettingsChanged, gameclients, this, &AthenaPlugin::Hook_ClientSettingsChanged, false);
+	SH_REMOVE_HOOK_MEMFUNC(IServerGameClients, ClientConnect, gameclients, this, &AthenaPlugin::Hook_ClientConnect, false);
+	SH_REMOVE_HOOK_MEMFUNC(IServerGameClients, ClientCommand, gameclients, this, &AthenaPlugin::Hook_ClientCommand, false);
+	SH_REMOVE_HOOK_MEMFUNC(IGameEventManager2, FireEvent, gameevents, this, &AthenaPlugin::Hook_FireEvent, false);
+
+	return true;
+}
+
+void AthenaPlugin::OnVSPListening(IServerPluginCallbacks *iface)
+{
+	vsp_callbacks = iface;
+}
+
+void AthenaPlugin::Hook_ServerActivate(edict_t *pEdictList, int edictCount, int clientMax)
+{
+	META_LOG(g_PLAPI, "ServerActivate() called: edictCount = %d, clientMax = %d", edictCount, clientMax);
+}
+
+void AthenaPlugin::AllPluginsLoaded()
+{
+	/* This is where we'd do stuff that relies on the mod or other plugins
+	 * being initialized (for example, cvars added and events registered).
+	 */
+}
+
+void AthenaPlugin::Hook_ClientActive(edict_t *pEntity, bool bLoadGame)
+{
+	META_LOG(g_PLAPI, "Hook_ClientActive(%d, %d)", IndexOfEdict(pEntity), bLoadGame);
+}
+
+#if SOURCE_ENGINE >= SE_ORANGEBOX
+void AthenaPlugin::Hook_ClientCommand(edict_t *pEntity, const CCommand &args)
+#else
+void AthenaPlugin::Hook_ClientCommand(edict_t *pEntity)
+#endif
+{
+#if SOURCE_ENGINE <= SE_DARKMESSIAH
+	CCommand args;
+#endif
+
+	if (!pEntity || pEntity->IsFree())
+	{
+		return;
+	}
+
+	const char *cmd = args.Arg(0);
+	META_CONPRINTF("%s: %s\n", GetLogTag(), cmd);
+}
+
+void AthenaPlugin::Hook_ClientSettingsChanged(edict_t *pEdict)
+{
+	if (playerinfomanager)
+	{
+		IPlayerInfo *playerinfo = playerinfomanager->GetPlayerInfo(pEdict);
+
+		const char *name = engine->GetClientConVarValue(IndexOfEdict(pEdict), "name");
+
+		if (playerinfo != NULL
+			&& name != NULL
+			&& strcmp(engine->GetPlayerNetworkIDString(pEdict), "BOT") != 0
+			&& playerinfo->GetName() != NULL
+			&& strcmp(name, playerinfo->GetName()) == 0)
+		{
+			char msg[128];
+			MM_Format(msg, sizeof(msg), "Your name changed to \"%s\" (from \"%s\")\n", name, playerinfo->GetName());
+			engine->ClientPrintf(pEdict, msg);
+		}
+	}
+}
+
+bool AthenaPlugin::Hook_ClientConnect(edict_t *pEntity,
+									const char *pszName,
+									const char *pszAddress,
+									char *reject,
+									int maxrejectlen)
+{
+	META_LOG(g_PLAPI, "Hook_ClientConnect(%d, \"%s\", \"%s\")", IndexOfEdict(pEntity), pszName, pszAddress);
+
+	return true;
+}
+
+void AthenaPlugin::Hook_ClientPutInServer(edict_t *pEntity, char const *playername)
+{
+
+}
+
+void AthenaPlugin::Hook_ClientDisconnect(edict_t *pEntity)
+{
+	META_LOG(g_PLAPI, "Hook_ClientDisconnect(%d)", IndexOfEdict(pEntity));
+}
+
+void AthenaPlugin::Hook_GameFrame(bool simulating)
+{
+	/**
+	 * simulating:
+	 * ***********
+	 * true  | game is ticking
+	 * false | game is not ticking
+	 */
+}
+
+bool AthenaPlugin::Hook_LevelInit(const char *pMapName,
+								char const *pMapEntities,
+								char const *pOldLevel,
+								char const *pLandmarkName,
+								bool loadGame,
+								bool background)
+{
+	META_LOG(g_PLAPI, "Hook_LevelInit(%s)", pMapName);
+
+	return true;
+}
+
+void AthenaPlugin::Hook_LevelShutdown()
+{
+	META_LOG(g_PLAPI, "Hook_LevelShutdown()");
+}
+
+void AthenaPlugin::Hook_SetCommandClient(int index)
+{
+	META_LOG(g_PLAPI, "Hook_SetCommandClient(%d)", index);
+}
+
+bool AthenaPlugin::Hook_FireEvent(IGameEvent *pEvent, bool bDontBroadcast)
+{
+	const char *name = pEvent->GetName();
+	if (strcmp(name, "player_say") == 0)
+	{
+		int userid = pEvent->GetInt("userid");
+		const char *text = pEvent->GetString("text");
+		if (stricmp(text, ".ready") == 0)
+		{
+			META_CONPRINTF("%s: Player (%d) is ready.\n", GetLogTag(), userid);
+		}
+	}
+
+	RETURN_META_VALUE(MRES_IGNORED, true);
+}
+
+bool AthenaPlugin::Pause(char *error, size_t maxlen)
+{
+	return true;
+}
+
+bool AthenaPlugin::Unpause(char *error, size_t maxlen)
+{
+	return true;
+}
+
+const char *AthenaPlugin::GetLicense()
+{
+	return "TBD";
+}
+
+const char *AthenaPlugin::GetVersion()
+{
+	return "0.0.1";
+}
+
+const char *AthenaPlugin::GetDate()
+{
+	return __DATE__;
+}
+
+const char *AthenaPlugin::GetLogTag()
+{
+	return "ATHENA";
+}
+
+const char *AthenaPlugin::GetAuthor()
+{
+	return "Kai";
+}
+
+const char *AthenaPlugin::GetDescription()
+{
+	return "Match Manager";
+}
+
+const char *AthenaPlugin::GetName()
+{
+	return "Athena";
+}
+
+const char *AthenaPlugin::GetURL()
+{
+	return "TBD";
+}
---
+++ athena_mm.h
@@ -0,0 +1,82 @@
+/**
+ * vim: set ts=4 sw=4 tw=99 noet :
+ */
+
+#ifndef _INCLUDE_METAMOD_SOURCE_STUB_PLUGIN_H_
+#define _INCLUDE_METAMOD_SOURCE_STUB_PLUGIN_H_
+
+#include <ISmmPlugin.h>
+#include <igameevents.h>
+#include <iplayerinfo.h>
+#include <sh_vector.h>
+#include "engine_wrappers.h"
+
+#if defined WIN32 && !defined snprintf
+#define snprintf _snprintf
+#endif
+
+enum MatchState
+{
+	WARMUP,
+	FIRST_HALF,
+	HALFTIME,
+	SECOND_HALF,
+	OVERTIME,
+	FINAL
+};
+
+
+class AthenaPlugin : public ISmmPlugin, public IMetamodListener, IGameEventListener2
+{
+public:
+	bool Load(PluginId id, ISmmAPI *ismm, char *error, size_t maxlen, bool late);
+	bool Unload(char *error, size_t maxlen);
+	bool Pause(char *error, size_t maxlen);
+	bool Unpause(char *error, size_t maxlen);
+	void AllPluginsLoaded();
+public: //IMetamodListener stuff
+	void OnVSPListening(IServerPluginCallbacks *iface);
+public: //hooks
+	void Hook_ServerActivate(edict_t *pEdictList, int edictCount, int clientMax);
+	bool Hook_LevelInit(const char *pMapName,
+		char const *pMapEntities,
+		char const *pOldLevel,
+		char const *pLandmarkName,
+		bool loadGame,
+		bool background);
+	void Hook_GameFrame(bool simulating);
+	void Hook_LevelShutdown(void);
+	void Hook_ClientActive(edict_t *pEntity, bool bLoadGame);
+	void Hook_ClientDisconnect(edict_t *pEntity);
+	void Hook_ClientPutInServer(edict_t *pEntity, char const *playername);
+	void Hook_SetCommandClient(int index);
+	void Hook_ClientSettingsChanged(edict_t *pEdict);
+	bool Hook_ClientConnect(edict_t *pEntity,
+		const char *pszName,
+		const char *pszAddress,
+		char *reject,
+		int maxrejectlen);
+#if SOURCE_ENGINE >= SE_ORANGEBOX
+	void Hook_ClientCommand(edict_t *pEntity, const CCommand &args);
+#else
+	void Hook_ClientCommand(edict_t *pEntity);
+#endif
+	bool Hook_FireEvent(IGameEvent *pEvent, bool bDontBroadcast);
+	void FireGameEvent(IGameEvent *pEvent) {};
+	int GetEventDebugID(void) { return 42; }
+public:
+	const char *GetAuthor();
+	const char *GetName();
+	const char *GetDescription();
+	const char *GetURL();
+	const char *GetLicense();
+	const char *GetVersion();
+	const char *GetDate();
+	const char *GetLogTag();
+};
+
+extern AthenaPlugin g_AthenaPlugin;
+
+PLUGIN_GLOBALVARS();
+
+#endif //_INCLUDE_METAMOD_SOURCE_STUB_PLUGIN_H_
