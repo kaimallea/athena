906b62d700cf01e81c0d9819120fd7f19c8c7206
Anton Vilhelm Ásgeirsson <anton@antonvilhelm.is>
2015-01-02 15:25:51 +0000

    fiddling around with events

--- event_handlers.h
+++ event_handlers.h
@@ -2,11 +2,18 @@
  * vim: set ts=4 sw=4 tw=99 noet:
 g*/
 
+#include<map>
+#include"igameevent.h"
+
 #ifndef ATHENA_EVENT_HANDLER
 #define ATHENA_EVENT_HANDLER
 
 class EventHandler
 {
+	private:
+		typedef void (*Callback)();
+		std::map <int, Callback> event_map;
+
 	int handler(IGameEvent *pEvent);
 	void player_death(IGameEvent *pEvent);
 };
